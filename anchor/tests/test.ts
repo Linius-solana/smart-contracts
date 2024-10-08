6386bcf499aa8595f5547ac48ff29a33c5ac1335
hoangquocvietuet <hoangquocvietuet@gmail.com>
2024-10-08 23:47:11 +0700

    first commit

---
+++ anchor/tests/test.ts
@@ -0,0 +1,48 @@
+import * as anchor from '@coral-xyz/anchor';
+import { Keypair } from '@solana/web3.js';
+import type { Social } from '../target/types/social';
+
+describe('Create Tokens', () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const payer = provider.wallet as anchor.Wallet;
+  const program = anchor.workspace.Social as anchor.Program<Social>;
+
+  const metadata = {
+    name: 'Solana Gold',
+    symbol: 'GOLDSOL',
+    uri: 'https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/spl-token.json',
+  };
+
+  it('Create an SPL Token!', async () => {
+    // Generate new keypair to use as address for mint account.
+    const mintKeypair = new Keypair();
+
+    // SPL Token default = 9 decimals
+    const transactionSignature = await program.methods
+      .createTokenMint(9, metadata.name, metadata.symbol, metadata.uri)
+      .accounts({
+        payer: payer.publicKey,
+        mintAccount: mintKeypair.publicKey,
+      })
+      .signers([mintKeypair])
+      .rpc();
+
+    console.log('Success!');
+    console.log(`   Mint Address: ${mintKeypair.publicKey}`);
+    console.log(`   Transaction Signature: ${transactionSignature}`);
+  });
+
+  it('post a post', async () => {
+    const transactionSignature = await program.methods.post('Hello, World!', 'Hello, World!')
+    .accounts({
+      payer: payer.publicKey,
+    })
+    .signers([payer.payer])
+    .rpc();
+
+    console.log('Success!');
+    console.log(`   Transaction Signature: ${transactionSignature}`);
+    
+  });
+});
